0800764ca3ff6afdc7fc5ed9fb7ab1c1f0cac52c
MuhammadAbdullah-coder997 <abdullahiqbal2008@gmail.com>
2026-01-29 15:23:24 +0500

    feat(frontend): created navbar component and added the logo in assests

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import { logo } from "../assets/logo"
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between px-8 py-4 bg-gray-900 text-white shadow-md">
+        <div>
+            <img src={logo} alt="RoadSight logo" className="h-10 w-10" />
+            <span className="font-bold text-xl">RoadSight</span>
+        </div>
+        <div>
+            <Link to="/" className="hover:text-gray-300" > Home </Link>
+            <Link to="/upload" className="hover:text-gray-300" > Upload </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
